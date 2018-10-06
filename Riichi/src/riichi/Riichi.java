efe2ac440b820c976ec022a56d805610448417c1
Tiia Vastamäki <Tiia Vastamki@Haapa.home>
2018-10-06 11:53:18 +0300

    Riichi luokka (testi)

---
+++ Riichi/src/riichi/Riichi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riichi;
+
+/**
+ *
+ * @author Tiia Vastamäki
+ */
+public class Riichi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
